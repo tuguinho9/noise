67c54b43265c932867624b9eb7fb0709fe6f0032
tuguinho9 <113730134+tuguinho9@users.noreply.github.com>
2023-10-13 18:14:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/bkaDrKcBl/model.json", modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResults);
+}
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+        document.getElementById("result_label").innerHTML = "Posso ouvir " + results[0].label;
+        document.getElementById("result_confidence").innerHTML = "Precisão - " + (results[0].confidence*100).toFixed(2) + "%";
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
+        document.getElementById("result_confidence").style.color = "rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
+        img1 = document.getElementById("alien1");
+        img2 = document.getElementById("alien2");
+        img3 = document.getElementById("alien3");
+        img4 = document.getElementById("alien4");
+        if(results[0].label=="Palmas"){
+            img1.src = "aliens-01.gif";
+            img2.src = "aliens-02.png";
+            img3.src = "aliens-03.png";
+            img4.src = "aliens-04.png";
+        }
+        else if(results[0].label=="Ruído de fundo") {
+            img1.src = "aliens-01.png";
+            img2.src = "aliens-02.gif";
+            img3.src = "aliens-03.png";
+            img4.src = "aliens-04.png";
+        }
+        else if(results[0].label=="Estalos") {
+            img1.src = "aliens-01.png";
+            img2.src = "aliens-02.png";
+            img3.src = "aliens-03.gif";
+            img4.src = "aliens-04.png";
+        }
+        else if(results[0].label=="Sino") {
+            img1.src = "aliens-01.png";
+            img2.src = "aliens-02.png";
+            img3.src = "aliens-03.png";
+            img4.src = "aliens-04.gif";
+        }
+    }
+}
